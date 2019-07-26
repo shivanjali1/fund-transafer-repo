5f5960c3a01955ea1be5d009c96a214a79875a6d
sontrive <sontrive0310@gmail.com>
2019-07-26 15:03:55 +0530

    otp files

---
+++ src/main/java/com/hcl/springbootbankapp/service/OTPService.java
@@ -0,0 +1,11 @@
+package com.hcl.springbootbankapp.service;
+
+import com.hcl.springbootbankapp.entity.OtpDetails;
+import com.hcl.springbootbankapp.exception.ApplicationException;
+
+public interface OTPService {
+	
+	OtpDetails generateOTP(String custId,Long tranId,String emailId);
+	boolean validate(String custId, Long tranId,String otpToValidate) throws ApplicationException;
+
+}
