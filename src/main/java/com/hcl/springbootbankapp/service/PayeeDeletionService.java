c3529cf984d084436c251a57634c3a73b14ff5e7
sontrive <sontrive0310@gmail.com>
2019-07-26 14:18:18 +0530

    change commit

---
+++ src/main/java/com/hcl/springbootbankapp/service/PayeeDeletionService.java
@@ -0,0 +1,7 @@
+package com.hcl.springbootbankapp.service;
+
+
+
+public interface PayeeDeletionService {
+
+}
---
+++ src/main/java/com/hcl/springbootbankapp/serviceimpl/PayeeDeletionServiceImpl.java
@@ -0,0 +1,8 @@
+package com.hcl.springbootbankapp.serviceimpl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PayeeDeletionServiceImpl {
+
+}
