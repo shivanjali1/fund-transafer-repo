b6a5d7b43cb0a8490ad47f646993ebf97dfbc497
shivanjali.kanase <kanaseshivanjali@gmail.com>
2019-07-26 17:33:36 +0530

    initial commit.

--- src/main/java/com/hcl/springbootbankapp/controller/AddPayeeController.java
+++ src/main/java/com/hcl/springbootbankapp/controller/AddPayeeController.java
@@ -3,15 +3,13 @@ package com.hcl.springbootbankapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import com.hcl.springbootbankapp.DTO.ResponseDTO;
+import com.hcl.springbootbankapp.dto.ResponseDTO;
 import com.hcl.springbootbankapp.exception.ApplicationException;
 import com.hcl.springbootbankapp.service.AddPayeeService;
 
