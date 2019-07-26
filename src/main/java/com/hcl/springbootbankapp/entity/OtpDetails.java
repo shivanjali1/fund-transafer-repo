e685454cfe56eaa28b5317c846c316980e8da91c
shivanjali.kanase <kanaseshivanjali@gmail.com>
2019-07-26 13:22:11 +0530

    initial commit.

---
+++ src/main/java/com/hcl/springbootbankapp/entity/OtpDetails.java
@@ -0,0 +1,40 @@
+package com.hcl.springbootbankapp.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.Data;
+
+@Data
+@Entity
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "optId")
+public class OtpDetails implements Serializable {
+	
+	private static final long serialVersionUID = -8985679445647579333L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "opt_id")
+	private  Long optId;
+	
+	private String customerId;
+	
+	@Column(name = "time")
+	private LocalDateTime time;
+	
+	@Column(name = "otp_code")
+	private String otpCode;
+	
+	@Column(name = "ref_id")
+	private Long refId;
+
+}
---
+++ src/main/java/com/hcl/springbootbankapp/entity/Payee.java
@@ -0,0 +1,26 @@
+package com.hcl.springbootbankapp.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "payee")
+public class Payee {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "first_name", nullable = false)
+	private Long custId;
+
+	@Column(name = "last_name",  nullable = false)
+	private Long payeeId;
+
+
+}
