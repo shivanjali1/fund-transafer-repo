6b88eafa8761dd53b5b02d252b16c244327f480f
sagar7gaikwad <sagar7gaikwad@gmail.com>
2019-07-26 15:57:08 +0530

    add payee

--- src/main/java/com/hcl/springbootbankapp/service/PayeeDeletionService.java
+++ src/main/java/com/hcl/springbootbankapp/service/PayeeDeletionService.java
@@ -1,6 +1,6 @@
 package com.hcl.springbootbankapp.service;
 
-
+import org.springframework.stereotype.Service;
 
 public interface PayeeDeletionService {
 
--- src/main/java/com/hcl/springbootbankapp/serviceimpl/AddPayeeServiceImpl.java
+++ src/main/java/com/hcl/springbootbankapp/serviceimpl/AddPayeeServiceImpl.java
@@ -5,28 +5,38 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.hcl.springbootbankapp.DTO.ResponseDTO;
+import com.hcl.springbootbankapp.entity.Payee;
 import com.hcl.springbootbankapp.entity.User;
+import com.hcl.springbootbankapp.repository.PayeeRepository;
 import com.hcl.springbootbankapp.repository.UserRepository;
 import com.hcl.springbootbankapp.service.AddPayeeService;
+import com.hcl.springbootbankapp.util.PayeeStatusUtil;
 @Service
 public class AddPayeeServiceImpl implements AddPayeeService {
 
 	@Autowired
 	UserRepository userRepository;
 	
+	@Autowired
+	PayeeRepository payeeRepository;
+	
 	@Override
 	public ResponseDTO addPayee(String loggedUserId, String payeeUserId) {
 		// TODO Auto-generated method stub
 		
 		User payeeUser = userRepository.findByCustomerId(payeeUserId);
-		
 		User loggedUser = userRepository.findByCustomerId(loggedUserId);
 		
+		Payee payee = new Payee();
+		payee.setStatus(PayeeStatusUtil.ADD_PENDING);
+		payee.setPayeeId(payeeUser.getId());
+		payee.setCustId(loggedUser.getId());
+		payeeRepository.save(payee);
 		
 		ResponseDTO responseObject = new ResponseDTO();
 		responseObject.setHttpStatus(HttpStatus.OK);
 		responseObject.setData(payeeUser);
-		responseObject.setMessage("Success");
+		responseObject.setMessage("Transaction Initiated");
 		
 		
 		return responseObject;
